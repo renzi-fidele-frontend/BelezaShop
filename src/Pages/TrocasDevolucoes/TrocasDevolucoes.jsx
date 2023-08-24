e5b851f537f6e3daea07414af83154c5e9fd26f0
Renzi Fidele <renzifidele2001@gmail.com>
2023-08-24 03:39:54 +0200

    fixLang e avancando com trocas e devolucoes

--- src/Pages/TrocasDevolucoes/TrocasDevolucoes.jsx
+++ src/Pages/TrocasDevolucoes/TrocasDevolucoes.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from "./TrocasDevolucoes.module.css";
 import estiloHome from "../Home/Home.module.css";
+import estiloBeleza from "../OculosBeleza/OculosBeleza.module.css";
 
 //Icons
-import { HiOutlineDocument } from "react-icons/hi";
+import { IoDocumentTextOutline } from "react-icons/io5";
 
 const TrocasDevolucoes = () => {
    return (
       <div id={styles.ct}>
          <div id={styles.cima}>
-            <HiOutlineDocument />
+            <IoDocumentTextOutline />
             <h5 className={estiloHome.tit}>Política de vendas.</h5>
             <p className={estiloHome.subtit}>Justa e sem burocracia.</p>
          </div>
@@ -17,7 +18,7 @@ const TrocasDevolucoes = () => {
          <div id={styles.body}>
             {/*Secao1 */}
             <div className={styles.secao}>
-               <h5>Garantia</h5>
+               <h5 className={estiloBeleza.tit2}>Garantia</h5>
                <p>
                   Nós temos a nossa maneira de garantir que todos os produtos da Jouse serão confeccionados com excelência, por isso, realmente
                   acreditamos que eles farão parte da sua vida por muito tempo.
@@ -39,14 +40,14 @@ const TrocasDevolucoes = () => {
 
             {/*Secao2 */}
             <div className={styles.secao}>
-               <h5>Garantia</h5>
+               <h5 className={estiloBeleza.tit2}>Garantia</h5>
                <p></p>
                <div></div>
             </div>
 
             {/*Secao3 */}
             <div className={styles.secao}>
-               <h5>Garantia</h5>
+               <h5 className={estiloBeleza.tit2}>Garantia</h5>
                <p></p>
                <div></div>
             </div>
