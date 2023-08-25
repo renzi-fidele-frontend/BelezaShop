c79c5cc039be6007a123549521ab5ec90138bc40
Renzi Fidele <renzifidele2001@gmail.com>
2023-08-25 03:55:55 +0200

    Sesao 1 e sesao2 terminadas

--- src/Pages/TrocasDevolucoes/TrocasDevolucoes.jsx
+++ src/Pages/TrocasDevolucoes/TrocasDevolucoes.jsx
@@ -3,8 +3,12 @@ import styles from "./TrocasDevolucoes.module.css";
 import estiloHome from "../Home/Home.module.css";
 import estiloBeleza from "../OculosBeleza/OculosBeleza.module.css";
 
+import small from "../../assets/smallGlass.png";
+
 //Icons
 import { IoDocumentTextOutline } from "react-icons/io5";
+import { BsBoxSeam } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const TrocasDevolucoes = () => {
    return (
@@ -30,19 +34,55 @@ const TrocasDevolucoes = () => {
                </p>
                <div>
                   <div id={styles.left}>
-                     <img src="" alt="" />
+                     <img src={small} alt="oculosBeleza" />
                   </div>
                   <div id={styles.right}>
-                     <h5></h5>
+                     <h5>Beleza All Black</h5>
+                     <p className={styles.subtit}>
+                        Garantia de <span>2 anos</span>
+                     </p>
+                     <br />
+                     <p>
+                        Nos comprometemos a reparar ou substituir as peças que apresentarem qualquer eventual problema. Não estão dentro das
+                        conformidades da garantia da Beleza problemas consequentes de mau uso, como arranhões por objetos pontudos, manchas
+                        causadas por químicos, danos causados por umidade, etc.
+                     </p>
                   </div>
                </div>
             </div>
 
             {/*Secao2 */}
             <div className={styles.secao}>
-               <h5 className={estiloBeleza.tit2}>Garantia</h5>
+               <h5 className={estiloBeleza.tit2}>Trocas e devoluções</h5>
                <p></p>
-               <div></div>
+               <div>
+                  <div id={styles.left}>
+                     {" "}
+                     <BsBoxSeam size={30} />
+                  </div>
+                  <div id={styles.right}>
+                     <h2 className={styles.secTit}>Solicite uma troca ou devolução</h2>
+                     <Link className={styles.secLink}>Fale com a equipe de suporte da Beleza ›</Link>
+                  </div>
+               </div>
+               <br /><br />
+               <p>
+                  Nós criamos peças de excelência porque realmente queremos que você adore ser nosso cliente. Entendemos, porém, que um produto
+                  comprado online pode não ser exatamente como você esperava, por isso, lhe reservamos o direito de solicitar uma troca ou
+                  devolução por qualquer motivo. Sem formulários, sem burocracia e sem enrolação.
+               </p>
+               <br />
+               <p>
+                  O prazo para você solicitar uma troca ou devolução é de sete dias após a entrega do seu pedido. Você pode optar pela troca
+                  grátis ou solicitar a devolução, sem nenhum custo adicional, para ser totalmente reembolsado.
+               </p>
+               <br />
+               <p>
+                  Para que a sua solicitação de troca ou devolução seja aceita, é imprescindível que seu pedido esteja em perfeitas condições,
+                  sem marcas ou indícios de mau uso. Se você optar pela devolução do pedido, seu pagamento será imediatamente reembolsado assim
+                  que seu pedido retornar para a Beleza e for cancelado. Pagamentos por cartão de crédito serão reembolsados na própria fatura e
+                  pagamentos por Pix serão reembolsados via Pix ou TED em até um dia útil após o cancelamento do pedido.
+               </p>
             </div>
 
             {/*Secao3 */}
